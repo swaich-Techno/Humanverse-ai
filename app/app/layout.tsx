5a5ac93f93a765c45d7edb8930d46ec28186e13d
swaich-Techno <swaichtecho@gmail.com>
2026-04-22 08:09:20 -0700

    Create layout.tsx

---
+++ app/app/layout.tsx
@@ -0,0 +1,15 @@
+export const metadata = {
+  title: "Humanverse AI"
+}
+
+export default function Rootlayout({
+  children,
+} : {
+  Children: React.ReactNode
+}) {
+  return (
+    <html>
+    <body>(children)</body>
+    </html>
+    )
+}
