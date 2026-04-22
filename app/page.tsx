817275ec0821614d811eba0ee5a80e4d5a4dedce
swaich-Techno <swaichtecho@gmail.com>
2026-04-22 07:56:27 -0700

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,25 @@
+export default function Home() {
+  return (
+    <main style={{
+      minHeight: "100vh",
+      background: "#000",
+      color: "#fff",
+      display: "flex",
+      fkexDirection: "column",
+      justifyContent: "centre",
+      alignItems: "centre",
+      fontFamily: "Arial"
+    }}>
+      <h1
+        style= {{fontSize: "64px", marginBottom: "10px"
+                }}>
+        HumanVerse AI
+      </h1>
+
+      <p
+        style= {{fontSize: "20px", color: "#999"}}>
+        Create Real Life AI Characters & Videos
+      </p>
+    </main>
+    )
+}
